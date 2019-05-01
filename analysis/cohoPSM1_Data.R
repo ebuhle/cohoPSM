6867b3f0343b4fc2d91867c9f1d3eb9196ed652c
ebuhle <eric.buhle@noaa.gov>
2019-04-30 21:16:10 -0700

    Break script into 3: (1) data wrangling (2) Feist et al. 2018 (3) New TNC analyses

---
+++ analysis/cohoPSM1_Data.R
@@ -0,0 +1,92 @@
+library(here)
+
+#==================================================================
+# DATA
+#==================================================================
+
+#----------------------------------------------
+# PSM SAMPLING SITES FOR ANALYSIS
+#----------------------------------------------
+
+# read in spawner data
+spawner_data <- read.csv(here("data","spawner_data.csv"), header=T)
+
+# read in landscape data
+spatial_data <- read.csv(here("data","spatial_data.csv"), header=T)
+
+# LU/LC and roads data
+lulc_roads_data <- spatial_data[,c("site","watershed","area","ccap_decid","ccap_open","ccap_evgrn",
+                                   "ccap_hdev","ccap_ldev","ccap_mdev","ccap_mxforest","ccap_wetland","ccap_ag",
+                                   "ccap_other","roads1","roads2","roads3","roads4","roads5","traffic",
+                                   "nlcd_imperv","restoration","pop_census","pop_lscan")]
+
+# convert basin area from m2 to ha
+lulc_roads_data$area <- lulc_roads_data$area/1e4
+
+# convert to km of road/km2
+lulc_roads_data[,grep("roads", names(lulc_roads_data))] <- lulc_roads_data[,grep("roads", names(lulc_roads_data))]/1000
+
+# Pretty variable names for LU/LC and roads
+lulc_roads_labels <- read.csv(here("data","lulc_roads_labels.csv"), header=T)
+lulc_roads_labels$plot_label <- ordered(as.character(lulc_roads_labels$plot_label),
+                                        levels = c("Imperviousness","High developed","Medium developed",
+                                                   "Low developed","Evergreen","Deciduous","Mixed forest",
+                                                   "Open space","Wetland","Agriculture","Interstate",
+                                                   "Principal arterial","Minor arterial","Collector arterial",
+                                                   "Local roads","Traffic intensity","Restoration",
+                                                   "U.S. Census","LandScan"))
+
+# matrix of annual summer and fall precip with sites and years as rows
+ppt_data <- spatial_data[,grep("site|ppt_su_2|ppt_fa_2", names(spatial_data))]
+ppt_su_cols <- grepl("ppt_su_2", names(ppt_data))
+ppt_fa_cols <- grepl("ppt_fa_2", names(ppt_data))
+n_yrs_ppt <- sum(ppt_su_cols)
+
+ppt_data <- data.frame(site = rep(ppt_data$site, each = n_yrs_ppt), 
+                       year = rep(substring(names(ppt_data)[ppt_su_cols], 8), nrow(ppt_data)),
+                       ppt_su = as.vector(t(as.matrix(ppt_data[,ppt_su_cols]))),
+                       ppt_fa = as.vector(t(as.matrix(ppt_data[,ppt_fa_cols]))))
+
+rm(list=c("ppt_su_cols","ppt_fa_cols","n_yrs_ppt"))
+
+# merge basin-specific variables and annual su and fa ppt into spawner data
+psm <- merge(spawner_data[,-1], ppt_data, by = c("site","year"))
+psm <- merge(psm, lulc_roads_data, by = "site")
+psm <- data.frame(psm[,c("site","watershed","year","n","n_psm","ppt_su","ppt_fa","area")], psm[,-(1:9)])
+psm <- psm[order(psm$site,psm$year),]
+
+#----------------------------------------------
+# ADDITIONAL SITES FOR PSM PREDICTIONS
+#----------------------------------------------
+
+# read in landscape data
+spatial_data_pre <- read.csv(here("data","spatial_data_predict.csv"), header=T)
+names(spatial_data_pre) <- gsub("ID", "site", names(spatial_data_pre))
+spatial_data_pre$watershed <- NA
+
+# convert basin area from m2 to ha
+spatial_data_pre$area <- spatial_data_pre$area/1e4
+
+# convert to km of road/km2
+spatial_data_pre[,grep("roads", names(spatial_data_pre))] <- spatial_data_pre[,grep("roads", names(spatial_data_pre))]/1000
+
+# only include basins w/ coho
+spatial_data_pre <- spatial_data_pre[spatial_data_pre$coho==1,]  
+spatial_data_pre$site <- factor(spatial_data_pre$site)
+
+lulc_roads_data_pre <- spatial_data_pre[,names(lulc_roads_data)]
+
+# Combine PSM-sampling and unsampled-site data frames
+lulc_roads_data_all <- rbind(data.frame(data = "psm", lulc_roads_data), 
+                             data.frame(data = "pre", lulc_roads_data_pre))
+
+# Add rows to psm for unobserved sites, plus one extra row for each observed site
+# containing no PSM data and having ppt set to the mean. These extra rows are 
+# used to generate predictions for monitored sites under baseline precip conditions.
+psm_all <- data.frame(lulc_roads_data_all, year = NA, n = 0, n_psm = 0,
+                      ppt_su = mean(psm$ppt_su), ppt_fa = mean(psm$ppt_fa))
+psm_all$data <- "pre"
+psm_all <- psm_all[,c("data", names(psm))]
+psm_all <- rbind(data.frame(data = "psm", psm), psm_all)
+psm_all <- data.frame(ID = 1:nrow(psm_all), psm_all)
+
---
+++ analysis/cohoPSM3_TNC.R
@@ -0,0 +1,66 @@
+if(.Platform$OS.type == "windows") options(device=windows)
+library(rstan)
+library(loo)
+library(shinystan)
+library(rstanarm)
+library(Hmisc)
+library(corrplot)
+library(here)
+source(here("analysis","stan_mean.R"))
+source(here("analysis","extract1.R"))
+source(here("analysis","cohoPSM1_data.R"))  # read and wrangle data
+
+
+#==================================================================
+# HIERARCHICAL REGRESSION MODELS FOR LANDSCAPE DATA AND PSM
+#==================================================================
+
+# Modify dataset
+# precip, traffic and log(traffic) centered and scaled to SD = 1
+psm_all_reg <- transform(psm_all, ppt_su = scale(ppt_su), ppt_fa = scale(ppt_fa),
+                         traffic = scale(traffic), log_traffic = scale(log(pmax(traffic, 0.1))))
+
+# Fit full model
+glmm_psm <- stan_glmer(cbind(n_psm, n - n_psm) ~ (ppt_su + ppt_fa) * log_traffic + 
+                         (ppt_su + ppt_fa || site) + (1 | ID),
+                       data = psm_all_reg, subset = data == "psm",
+                       family = binomial("logit"),
+                       prior_intercept = normal(0,3),
+                       prior = normal(0,3),
+                       prior_covariance = decov(),
+                       chains = 3, cores = 3, iter = 2000, warmup = 1000,
+                       control = list(adapt_delta = 0.9))
+
+glmm_psm
+summary(glmm_psm, prob = c(0.025, 0.5, 0.975), pars = "beta", include = FALSE)
+
+# Calculate marginal log-likelihood, marginalizing over obs-level random errors
+# (to do this, specify new levels of ID)
+newdat <- psm_all_reg[psm_all_reg$data == "psm",]
+newdat <- data.frame(idx = rep(1:nrow(newdat), each = 500), 
+                     newdat[rep(1:nrow(newdat), each = 500),])
+newdat$ID <- max(newdat$ID) + 1:nrow(newdat)
+### WTF? posterior_linpred() ignores ID-level intercept (but works with toy example)
+ll_glmm_psm <- t(posterior_linpred(glmm_psm, newdata = newdat, transform = TRUE))
+ll_glmm_psm <- apply(ll_glmm_psm, 1, function(p) dbinom(newdat$n_psm, newdat$n, p))
+ll_glmm_psm <- aggregate(ll_glmm_psm, by = list(idx = newdat$idx), mean)
+
+### just for kicks
+load(here("results","stan_psm.RData"))  # saved stanfit object from next code chunk below
+psm_all_reg$Z[psm_all_reg$data == "psm"] <- stan_mean(stan_psm,"Z")[as.numeric(psm$site)]
+
+glmm_psm_Z <- stan_glmer(cbind(n_psm, n - n_psm) ~ (ppt_su + ppt_fa) * Z + 
+                           (ppt_su + ppt_fa || site) + (1 | ID),
+                         data = psm_all_reg, subset = data == "psm",
+                         family = binomial("logit"),
+                         prior_intercept = normal(0,3),
+                         prior = normal(0,3),
+                         prior_covariance = decov(),
+                         chains = 3, cores = 3, iter = 2000, warmup = 1000,
+                         control = list(adapt_delta = 0.9))
+
+glmm_psm_Z
+summary(glmm_psm_Z, prob = c(0.025, 0.5, 0.975), pars = "beta", include = FALSE)
+###
+
+
