2621dae25c089d6653a23ac7d1fda45bc01d3338
ebuhle <eric.buhle@noaa.gov>
2019-05-02 10:47:29 -0700

    Debug stan_init_cv()

--- analysis/cohoPSM2_Roads2Ruin.R
+++ analysis/cohoPSM2_Roads2Ruin.R
@@ -33,7 +33,7 @@ if(file.exists(here("analysis","results","stan_psm_all.RData"))) {
   load(here("analysis","results","stan_psm_all.RData"))
   eval_stan_psm_all <- FALSE
 }
-#+ ignore
+#+ include = FALSE
 
 #==================================================================
 # STRUCTURAL EQUATION MODELS FOR LANDSCAPE DATA AND PSM
@@ -102,14 +102,14 @@ stan_dat <- list(S = nrow(X),
                  n_psm = psm$n_psm,
                  I_fit = rep(1, nrow(psm)),
                  I_lpd = rep(1, nrow(psm)))
-## @ knitr ignore
+## @knitr ignore
 
 
 #'------------------------------------------------------
 #' Function to generate initial values for SEM
 #'------------------------------------------------------
 #+ stan_init
-## @ knitr
+## @knitr
 
 # Function to generate initial values for chains
 stan_init <- function(stan_dat) 
@@ -248,7 +248,7 @@ for(i in 1:length(stan_psm_list))
 }
 
 stan_psm_mods <- stan_psm_mods[order(stan_psm_mods$dLOO),]                            
-stan_psm_mods[order(stan_psm_mods$dWAIC),]
+format(stan_psm_mods, nsmall = 2, digits = 2)
 
 # Save objects
 save(stan_psm_list, stan_psm_mods, file = here("analysis","results","stan_psm_WAIC_LOO.RData"))
@@ -266,25 +266,30 @@ stan_init_cv <- function(fit)
   M <- nrow(samples$a0)
   i <- sample(M,1)
   
+  A <- matrix(samples$A[i,,], nrow = dim(samples$A)[2], ncol = dim(samples$A)[3], byrow = TRUE)
+  A <- A[lower.tri(A), diag = TRUE]
+  logit_p_psm_hat <- stan_mean(fit,"logit_p_psm_hat")
+  logit_p_psm <- qlogis(samples$p_psm[i,])
+
   with(samples, 
        list(a0 = a0[i,],
-            A_nid_vec = array(A_nid_vec[i,], dim = ncol(A_nid_vec)),
-            Z_nid = matrix(Z[i,,], nrow = dim(Z)[2], ncol = dim(Z)[3]),
+            A_nid_vec = array(A, dim = length(A)),
+            Z_nid = matrix(Z[i,,], nrow = dim(Z)[2], ncol = dim(Z)[3], byrow = TRUE),
             phi = phi[i,],
             mu_b0 = mu_b0[i],
-            b0_Z_nid = array(b0_Z_nid[i,], dim = ncol(b0_Z_nid)),
+            b0_Z_nid = array(b0_Z[i,], dim = ncol(b0_Z)),
             sigma_b0 = sigma_b0[i],
-            b0_std = array(b0_std[i,], dim = ncol(b0_std)),
+            b0_std = array((b0[i,] - mu_b0[i]) / sigma_b0[i], dim = ncol(b0)),
             mu_b_su = mu_b_su[i],
-            b_su_Z_nid = array(b_su_Z_nid[i,], dim = ncol(b_su_Z_nid)),
+            b_su_Z_nid = array(b_su_Z[i,], dim = ncol(b_su_Z)),
             sigma_b_su = sigma_b_su[i],
-            b_su_std = array(b_su_std[i,], dim = ncol(b_su_std)),
+            b_su_std = array((b_su[i,] - mu_b_su[i]) / sigma_b_su[i], dim = ncol(b_su)),
             mu_b_fa = mu_b_fa[i],
-            b_fa_Z_nid = array(b_fa_Z_nid[i,], dim = ncol(b_fa_Z_nid)),
+            b_fa_Z = array(b_fa_Z[i,], dim = ncol(b_fa_Z)),
             sigma_b_fa = sigma_b_fa[i],
-            b_fa_std = array(b_fa_std[i,], dim = ncol(b_fa_std)),
+            b_fa_std = array((b_fa[i,] - mu_b_fa[i]) / sigma_b_fa[i], dim = ncol(b_fa)),
             sigma_psm = sigma_psm[i],
-            logit_p_psm_std = array(logit_p_psm_std[i,], dim = ncol(logit_p_psm_std))))
+            logit_p_psm_std = array((logit_p_psm - logit_p_psm_hat) / sigma_psm[i], dim = length(logit_p_psm))))
 }
 
 
