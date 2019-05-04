38934e4fe9446b8096737059a516935959830b50
ebuhle <eric.buhle@noaa.gov>
2019-05-04 16:28:04 -0700

    Still wrestling with linear predictor for new groups, now with fitted(brmsfit)

--- analysis/cohoPSM3_TNC.R
+++ analysis/cohoPSM3_TNC.R
@@ -63,7 +63,7 @@ summary(glmm_psm, prob = c(0.025, 0.5, 0.975), pars = "beta", include = FALSE, d
 #+ posterior_linpred_reprex
 
 dat <- data.frame(group = gl(10, 5), y = rnorm(10)[rep(1:10, each = 5)] + rnorm(50))
-lmm <- stan_lmer(y ~ 1 + (1|group), data = dat, seed = 666)
+lmm <- stan_lmer(y ~ 1 + (1|group), data = dat, chains = 1, seed = 666)
 lp1 <- posterior_linpred(lmm, newdata = data.frame(group = 9:12), re.form = NA)
 head(lp1)
 lp2 <- posterior_linpred(lmm, newdata = data.frame(group = 9:12), re.form = ~ (1|group))
@@ -76,17 +76,38 @@ median(as.matrix(lmm, regex_pars = "Sigma"))
 #' the group-level effects are set to `0` and the prediction for all groups is the hyper-mean 
 #' (`(Intercept)`). It's not clear what `re.form = NULL` is doing. It's not using the hyper-mean,
 #' but the two new groups are identical. Maybe it's drawing a *single* new group-level 
-#' intercept at each posterior *sample*? But the SD of the residuals differs from the hyper-SD,
-#' so...WTF? Looks like I'll have to put this one to the Stan forums.
+#' intercept at each posterior *sample*? Looks like I'll have to put this one to the Stan forums.
 #' 
 #' In the meantime, it's actually not that difficult to construct the new observation-level 
 #' residuals by drawing from their hyperdistribution conditional on each sample of the hyper-mean
-#' and hyper-SD.
+#' and hyper-SD. One approach would be to do this manually, adding the residuals
+#' to the output of `posterior_linpred()` called with `re.form` leaving out the
+#' observation-level term(s) in question. But that would entail figuring out the internal `lme4`
+#' `Z`-matrix plumbing. A less efficient but much simpler brute-force approach is to just call
+#' `posterior_predict()` repeatedly, once for each row in `newdata` with an unobserved group
+#' (since a different draw from the hyperdistribution is returned each time).
 
-#+ posterior_linpred_newgroups
+#+ brms_fitted_newgroups
 # Function to simulate from the posterior distribution of the linear predictor,
 # including group-varying intercepts for new groups not included in the fitted data
-## @knitr posterior_linpred_newgroups
+## @knitr brms_fitted_newgroups
+brms_fitted_newgroups <- function(object, newdata, re_formula = NULL) 
+{
+  lp <- matrix(NA, nrow(as.matrix(object)), nrow(newdata))
+  re <- ranef(object)
+  # rows of newdata that have new levels of any grouping factors
+  newgroups <- sapply(names(re), function(f) !(newdata[,f] %in% rownames(re[[f]])))
+  print(newgroups)
+  if(!all(newgroups))
+    lp[,!newgroups] <- fitted(object, newdata = newdata[!newgroups,,drop = FALSE], 
+                              re_formula = re_formula)
+  if(any(newgroups))
+    for(i in which(newgroups))
+      lp[,i] <- fitted(object, newdata = newdata[i,,drop = FALSE], 
+                             re_formula = re_formula, allow_new_levels = TRUE)
+  return(lp)
+}
+
 
 
 #+ glmm_psm_loglik
@@ -110,9 +131,13 @@ ll_glmm_psm <- aggregate(ll_glmm_psm, by = list(idx = newdat$idx), mean)
 #+ brms_predict_reprex
 
 dat <- data.frame(group = gl(10, 5), y = rnorm(10)[rep(1:10, each = 5)] + rnorm(50))
-lmm <- stan_lmer(y ~ 1 + (1|group), data = dat, seed = 666)
-lp1 <- posterior_linpred(lmm, newdata = data.frame(group = 9:12), re.form = NA)
+lmm <- brm(y ~ 1 + (1|group), data = dat, chains = 1)
+lmm
+lp1 <- fitted(lmm, newdata = data.frame(group = 9:12), re_formula = NA, summary = FALSE)
 head(lp1)
-lp2 <- posterior_linpred(lmm, newdata = data.frame(group = 9:12), re.form = ~ (1|group))
+lp2 <- fitted(lmm, newdata = data.frame(group = 9:12), re_formula = ~ (1|group), 
+               allow_new_levels = TRUE, summary = FALSE)
 head(lp2)
+lp3 <- brms_fitted_newgroups(lmm, newdata = data.frame(group = 9:12), re_formula = ~ (1|group))
+head(lp3)
 
