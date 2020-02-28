a7ed51f48522e06f5035580912cce164ac0c05ce
ebuhle <eric.buhle@noaa.gov>
2020-02-28 10:23:04 -0800

    Delete functions and Stan code included in SEMPSM

--- analysis/functions/extract1.R
+++
@@ -1,5 +0,0 @@
-# Convenience function to simplify extracting single parameters from stanfit objects
-extract1 <- function(object, par)
-{
-  extract(object, par)[[1]]
-}
--- analysis/functions/stan_mean.R
+++
@@ -1,6 +0,0 @@
-# Convenience function to simplify extracting posterior means from stanfit objects
-stan_mean <- function(object, pars)
-{
-  mm <- get_posterior_mean(object, pars)
-  return(mm[,ncol(mm)])
-}
